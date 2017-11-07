6108dd85f0042233694ebae7d3e992acfa8d76fe
Mathias Neerup <manee12@student.sdu.dk>
2017-11-07 09:24:09 +0100

    Script to do MES-testing

---
+++ src/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo -e  "GET ORDER 1\n" | nc -q 1 localhost 4567 | xxd -c1
+
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0001 0" | nc -q 1 localhost 4567
+
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0002 1" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0003 0" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0004 1" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0005 0" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0006 1" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0007 0" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0008 1" | nc -q 1 localhost 4567
+echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+echo -e  "POST PARTSTATUS\n0009 0" | nc -q 1 localhost 4567
+
+
+
+
+
+
+
+#echo -e "GET PART 1\n" | nc -q 1 localhost 4567  | xxd -c 1
+#echo -e  "POST PARTSTATUS\n0001 1 2 7 8" | nc -q 1 localhost 4567
+echo -e  "POST ORDERSTATUS\n0001 1 2 7 8\n" | nc -q 1 localhost 4567 
+
+
+
